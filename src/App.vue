8c539f124ff991f1a31fa2c1c74faf6fce1d09c3
liuyue <liuyue@soyoung.com>
2022-03-17 19:36:15 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>App组件</h1>
+  <p>姓名：{{name}}</p>
+  <p>年龄：{{age}}</p>
+  <button @click="sayHello">按钮</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    let name = "Tom";
+    let age = 23;
+
+    const sayHello = function() {
+      alert(`我叫${name},今年${age}岁。`);
+    };
+
+
+    return {
+      name,
+      age,
+      sayHello
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// 引入的不再是Vue构造函数了，引入的是一个名为createApp的工厂函数
 import { createApp } from 'vue'
 import App from './App.vue'
 
