d8dda26593a2f25f062cb5098919f824b2257f04
DESKTOP-N4UDOE8\JUne <2734259470@qq.com>
2022-03-17 23:35:08 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,33 @@
   <h1>App组件</h1>
   <p>姓名：{{name}}</p>
   <p>年龄：{{age}}</p>
-  <button @click="sayHello">按钮</button>
+  <p>工作种类：{{job.type}}</p>
+  <p>工作薪水：{{job.salary}}</p>
+  <button @click="changeInfo">修改信息</button>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
   name: 'App',
   setup() {
-    let name = "Tom";
-    let age = 23;
+    let name = ref("Tom");
+    let age = ref(23);
+    let job = ref({
+      type: "测试工程师",
+      salary: "0k",
+    });
 
-    const sayHello = function() {
-      alert(`我叫${name},今年${age}岁。`);
-    };
-
+  function changeInfo() {
+    name.value = "李四";
+    age.value = 48;
+  }
 
     return {
       name,
       age,
-      sayHello
+      job,
+      changeInfo,
     };
   }
 }
