1f67a7ac9a8b61ab7cd120ca5872b3afdabe7777
liuyue <liuyue@soyoung.com>
2022-03-21 18:55:15 +0800

    setup注意点

---
+++ 05-setup的注意点/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <PersonInfo @hello="showHelloMsg" age=20 />
+</template>
+
+<script>
+import PersonInfo from "./components/PersonInfo.vue"
+export default {
+  components: {
+    PersonInfo
+  },
+  setup(){
+    function showHelloMsg(value) {
+      alert(`Hello,接受到的数据为:${value}`)
+    }
+
+
+    return{
+      showHelloMsg
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
