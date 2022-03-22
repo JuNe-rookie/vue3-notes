ad4753efe1de5005643724f41a0f238e2ac3744c
liuyue <liuyue@soyoung.com>
2022-03-22 20:49:48 +0800

    Vue3生命周期钩子

---
+++ 08-Vue3生命周期钩子/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="isShowDemo = !isShowDemo">切换显示/隐藏</button>
+  <numberSum v-if="isShowDemo"/>
+</template>
+
+<script setup>
+import numberSum from "./components/numberSum.vue"
+import { ref } from "vue"
+
+let isShowDemo = ref(true)
+</script>
+
+<style>
+
+</style>
---
+++ 08-Vue3生命周期钩子/components/numberSum.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>当前求和为：{{ sum }}</h1>
+  <button @click="sum++">点击+1</button>
+</template>
+
+<script>
+import { ref, onBeforeMount } from "vue"
+export default {
+  setup() {
+    let sum = ref(0)
+    onBeforeMount(()=>{
+      console.log("----------onBeforeMount----------");
+    })
+
+    return {
+      sum
+    }
+  }
+}
+
+</script>
+
+
+<style lang="css">
+
+</style>
